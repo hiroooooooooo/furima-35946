3b775c8093aaf2c3eced14a4c0d31b7221e34c62
hiroooooooooo <piro.a1013@gmail.com>
2021-06-16 16:44:57 +0900

    userモデルのvalidates

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :last_kanji, presence: true
+  validates :first_kanji, presence: true
+  validates :last_kana, presence: true
+  validates :first_kana, presence: true
+  validates :birthday, presence: true
+  
 end
