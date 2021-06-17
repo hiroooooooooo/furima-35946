92fdc6bf12b803918b9c1ceb1e1500d659031db0
hiroooooooooo <piro.a1013@gmail.com>
2021-06-17 16:58:36 +0900

    エラーハンドリングの表示の成功と、Rspec導入

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,17 +4,24 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  VALID_PASSWORD_REGIX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
+  # validates :password, length: {minimum: 6}, format: {with:VALID_PASSWORD_REGIX, message: 'Include both letters and numbers'}
+  validates :password, format: { with:VALID_PASSWORD_REGIX, message: 'Include both letters and numbers' }
+
   with_options presence: true do
+
     validates :nickname
-    with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "全角（漢字・ひらがな・カタカナ）で入力してください" } do
+    validates :birthday
+
+    with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'Input full-width characters' } do
       validates :last_kanji
       validates :first_kanji
     end
-    with_options format: { with: /\A[ァ-ヶ]+\z/, message: "全角（カタカナ）で入力してください" } do
+    with_options format: { with: /\A[ァ-ヶ]+\z/, message: 'Input full-width katakana characters' } do
     validates :last_kana
     validates :first_kana
     end
-    validates :birthday
+    
   end
   
 end
