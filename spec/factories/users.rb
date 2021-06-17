0d16f5470dce4a252e23c0add3bbdd7708e175e8
hiroooooooooo <piro.a1013@gmail.com>
2021-06-18 01:27:46 +0900

    モデルの単体テストコードの記述

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,13 @@
 FactoryBot.define do
   factory :user do
     nickname              {'test'}
-    email                 {'test@example'}
-    password              {'000000'}
+    email                 {'test@com'}
+    password              {'test00'}
     password_confirmation {password}
+    last_kanji            {'村田'}
+    first_kanji           {'浩之'}
+    last_kana             {'ムラタ'}
+    first_kana            {'ヒロユキ'}
+    birthday              {'1983-05-19'}
   end
 end
