4b44edb7581223d7b20cc99c92b3b01a295d3da9
hiroooooooooo <piro.a1013@gmail.com>
2021-06-19 11:09:47 +0900

    バリデーションの追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,16 +1,23 @@
 class Item < ApplicationRecord
-  has_one_attached :image
-
-  extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :category
-  validates :category_id, numericality: { other_than: 1 }
-  belongs_to :status
-  validates :status_id, numericality: { other_than: 1 }
-  belongs_to :delivery_fee
-  validates :delivery_fee, numericality: { other_than: 1 }
-  belongs_to :prefecture
-  validates :prefecture, numericality: { other_than: 1 }
-  belongs_to :days_of_ship
-  validates :days_of_ship, numericality: { other_than: 1 }
   
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  
+  with_options presence: true do
+    validates :title
+    validates :description
+    validates :category_id, numericality: { other_than: 1 }
+    validates :status_id, numericality: { other_than: 1 }
+    validates :delivery_fee, numericality: { other_than: 1 }
+    validates :prefecture, numericality: { other_than: 1 }
+    validates :days_of_ship, numericality: { other_than: 1 }
+
+    validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
+  end
+
+  has_one_attached :image
+  belongs_to :category
+  belongs_to :status
+  belongs_to :delivery_fee
+  belongs_to :prefecture
+  belongs_to :days_of_ship
 end
