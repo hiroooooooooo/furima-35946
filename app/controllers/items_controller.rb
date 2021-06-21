2683ff926480239fa939fdfd159fbb53249cfed6
hiroooooooooo <piro.a1013@gmail.com>
2021-06-21 14:11:00 +0900

    修正完了

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_action :item_find, only: [:show, :edit, :update]
+  before_action :edit_secure, only: [:edit, :update]
 
   def index
     @item = Item.all.order('created_at DESC')
@@ -19,17 +21,17 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
+    # @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
-
-    redirect_to root_path if current_user.id != @item.user_id
+    # @item = Item.find(params[:id])
+    # redirect_to root_path if current_user.id != @item.user_id
   end
 
   def update
-    @item = Item.find(params[:id])
+    # @item = Item.find(params[:id])
+    # redirect_to root_path if current_user.id != @item.user_id
     if @item.update(item_params)
       redirect_to item_path(@item.id)
     else
@@ -38,6 +40,14 @@ class ItemsController < ApplicationController
   end
 
   private
+
+  def item_find
+    @item = Item.find(params[:id])
+  end
+
+  def edit_secure
+    redirect_to root_path if current_user.id != @item.user_id
+  end
 
   def item_params
     params.require(:item).permit(:title, :description, :category_id, :status_id, :delivery_fee_id,
